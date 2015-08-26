cbc657c2bad330b2e2fac6d028f9f5c43a04e353
Jack Firth <jackhfirth@gmail.com>
2015-08-26 16:05:03 -0700

    Remove implementation

--- exercises/ex2/src.js
+++ exercises/ex2/src.js
@@ -6,9 +6,6 @@ var isEven = function(x) { return x % 2 === 0 };
 
 var hasEvenAge = R.propSatisfies(isEven, "age");
 
-var getUsersWithEvenAge = R.compose(
-  R.filter(hasEvenAge),
-  R.prop("users")
-);
+var getUsersWithEvenAge;
 
 module.exports = getUsersWithEvenAge
