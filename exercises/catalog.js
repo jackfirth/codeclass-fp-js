be75eb15b424b99d9cf53254ae1ee79135b2ace9
Jack Firth <jackhfirth@gmail.com>
2015-08-25 22:25:40 -0700

    Add first exercise

---
+++ exercises/catalog.js
@@ -0,0 +1,12 @@
+var catalog = {
+  users: [
+    {id: 3, name: "Thad", age: 36},
+    {id: 5, name: "Lucian", age: 23},
+    {id: 2, name: "Justine", age: 29},
+    {id: 4, name: "Katie", age: 26},
+    {id: 0, name: "Jerold", age: 52},
+    {id: 1, name: "Nona", age: 33}
+  ]
+};
+
+module.exports = catalog;
---
+++ exercises/ex1/src.js
@@ -0,0 +1,6 @@
+var catalog = require('../catalog');
+var R = require('ramda');
+
+var sortCatalogUsersById;
+
+module.exports = sortCatalogUsersById
