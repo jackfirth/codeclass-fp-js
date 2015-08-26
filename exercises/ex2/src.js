3c8ad4bc583ed2c1ab697456a88666ef0fec13af
Jack Firth <jackhfirth@gmail.com>
2015-08-26 16:27:52 -0700

    Remove more leftover implementation

--- exercises/ex2/src.js
+++ exercises/ex2/src.js
@@ -1,11 +1,6 @@
 var catalog = require('../catalog');
 var R = require('ramda');
 
-var isEven = function(x) { return x % 2 === 0 };
-
-
-var hasEvenAge = R.propSatisfies(isEven, "age");
-
 var getUsersWithEvenAge;
 
 module.exports = getUsersWithEvenAge
